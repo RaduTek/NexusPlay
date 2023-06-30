36ec48eb9dd5c96d395e779cb615e2f2c47a5725
RaduTek <radutek13@gmail.com>
2023-06-30 17:13:33 +0300

    Add atoms for global playback control

---
+++ src/Atoms.js
@@ -0,0 +1,6 @@
+import { atom } from "jotai";
+
+export const playerStatus = atom(0);
+export const playerCurrentTime = atom(0);
+export const playerSeekTime = atom(0);
+export const playerTotalTime = atom(0);
