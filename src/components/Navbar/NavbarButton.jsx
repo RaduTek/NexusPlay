f74270456db2a1efee9ddbf143f898910349e61d
RaduTek <radutek13@gmail.com>
2023-06-30 17:14:28 +0300

    Update style

--- src/components/Navbar/NavbarButton.jsx
+++ src/components/Navbar/NavbarButton.jsx
@@ -1,10 +1,29 @@
 import Icon from "@mdi/react";
 import "./NavbarButton.css";
+import Tooltip from "../UI/Tooltip";
 
-function NavbarButton({ icon, children }) {
+function NavbarButton({
+    icon,
+    iconSize = 1.4,
+    tooltip = "",
+    tooltipPosition = "topLeft",
+    onClick,
+    children,
+}) {
     return (
-        <button className={"navbarButton" + (icon ? " icon" : "")}>
-            {icon ? <Icon path={icon} size={1.5} /> : <span>{children}</span>}
+        <button
+            className={"navbarButton" + (icon ? " icon tooltipHost" : "")}
+            onClick={onClick}
+            aria-label={children}
+        >
+            {icon ? (
+                <Icon path={icon} size={iconSize} />
+            ) : (
+                <span>{children}</span>
+            )}
+            {tooltip.length > 0 && (
+                <Tooltip position={tooltipPosition}>{children}</Tooltip>
+            )}
         </button>
     );
 }
