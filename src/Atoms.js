6c7b1efa9a7aee7add30ebaaffca967bfee134bc
RaduTek <radutek13@gmail.com>
2023-07-01 14:59:35 +0300

    Set playback status to pause by default

--- src/Atoms.js
+++ src/Atoms.js
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 
-export const playerStatus = atom(0);
+export const playerStatus = atom(1); // Paused
 export const playerCurrentTime = atom(0);
 export const playerSeekTime = atom(0);
 export const playerTotalTime = atom(0);
