fe1628589ccddcb8099c5a6e710dc5454360b96a
RaduTek <radutek13@gmail.com>
2023-07-01 23:14:33 +0300

    Implement drag to close Now Playing

--- src/components/Layout/NowPlaying/NowPlaying.jsx
+++ src/components/Layout/NowPlaying/NowPlaying.jsx
@@ -1,6 +1,7 @@
 import "./NowPlaying.css";
 import {
-    mdiChevronDown,
+    mdiChevronRight,
+    mdiDotsVertical,
     mdiHeartOutline,
     mdiPause,
     mdiPlay,
@@ -14,110 +15,194 @@ import {
 import CircleButton from "../../UI/CircleButton";
 import { TabPage, TabView } from "../TabView/TabView";
 import { useAtom } from "jotai";
-import { playerStatus, nowPlayingVisible } from "../../../Atoms";
+import {
+    playerStatus,
+    nowPlayingVisible,
+    nowPlayingDragY,
+} from "../../../Atoms";
+import SeekBar from "../../Player/SeekBar";
+import { useState } from "react";
+import { useWindowSize } from "@uidotdev/usehooks";
+import sampleAlbumArt from "../../../images//Album Art.jpg";
 
 function NowPlaying() {
     const [getPlayerStatus, setPlayerStatus] = useAtom(playerStatus);
     const [isVisible, setVisible] = useAtom(nowPlayingVisible);
+    const [dragStartY, setDragStartY] = useState(0);
+    const [dragCurrentY, setDragCurrentY] = useAtom(nowPlayingDragY);
+    const windowSize = useWindowSize();
+
+    const dragStart = (e) => {
+        if (window.innerWidth > 550) return;
+        setDragStartY(e.touches[0].clientY);
+        setDragCurrentY(0);
+    };
+    const dragMove = (e) => {
+        if (window.innerWidth > 550) return;
+        setDragCurrentY(e.changedTouches[0].clientY - dragStartY);
+    };
+    const dragEnd = (e) => {
+        if (window.innerWidth > 550) return;
+        if (
+            e.changedTouches[0].clientY - dragStartY >
+            window.innerHeight / 3.5
+        ) {
+            setVisible(false);
+        }
+        setDragCurrentY(0);
+        setDragStartY(0);
+    };
 
     return (
-        <div className={"nowPlaying " + (isVisible ? "" : "hidden")}>
-            <div className="topButtons">
-                <CircleButton
-                    icon={mdiChevronDown}
-                    iconOnly
-                    padding={4}
-                    onClick={() => {
-                        setVisible(false);
-                    }}
-                >
-                    Hide
-                </CircleButton>
+        <div
+            className={"nowPlaying " + (isVisible ? "" : "hidden")}
+            style={
+                dragCurrentY > 0
+                    ? {
+                          transform: "translateY(" + dragCurrentY + "px)",
+                          transition: "none",
+                      }
+                    : {}
+            }
+        >
+            <div
+                className="topButtons"
+                onTouchStart={dragStart}
+                onTouchMove={dragMove}
+                onTouchEnd={dragEnd}
+            >
+                <div className="buttonsLeft">
+                    <CircleButton
+                        className="nowPlayingHideBtn"
+                        icon={mdiChevronRight}
+                        iconSize={1.2}
+                        iconOnly
+                        padding={4}
+                        onClick={() => {
+                            setVisible(false);
+                        }}
+                    >
+                        Hide
+                    </CircleButton>
+                </div>
+                <div className="buttonsRight">
+                    <CircleButton
+                        icon={mdiDotsVertical}
+                        iconSize={1.2}
+                        iconOnly
+                        padding={4}
+                    >
+                        More Options
+                    </CircleButton>
+                </div>
             </div>
-            <TabView align="bottom">
-                <TabPage title="Now Playing" className="nowPlayingPage">
-                    <div className="albumArtList">
-                        <div className="albumArtFrame">
-                            <div className="albumArtInner">
-                                <img alt="Album Art" />
+            <TabView
+                align="bottom"
+                defaultPage={windowSize.width > 550 ? 1 : 0}
+            >
+                <TabPage
+                    title="Now Playing"
+                    className="nowPlayingPage"
+                    hidden={windowSize.width > 550}
+                >
+                    <div
+                        className="nowPlayingPage"
+                        onTouchStart={dragStart}
+                        onTouchMove={dragMove}
+                        onTouchEnd={dragEnd}
+                    >
+                        <div className="albumArtList">
+                            <div className="albumArtFrame">
+                                <div className="albumArtInner">
+                                    <img alt="Album Art" src={sampleAlbumArt} />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="songDetails">
-                        <div>
-                            <CircleButton
-                                icon={mdiPlaylistPlus}
-                                iconOnly
-                                iconSize={1.3}
-                            >
-                                Add to playlist
-                            </CircleButton>
+                        <div className="playbackControls">
+                            <div className="songDetails">
+                                <div>
+                                    <CircleButton
+                                        icon={mdiPlaylistPlus}
+                                        iconOnly
+                                        iconSize={1.3}
+                                    >
+                                        Add to playlist
+                                    </CircleButton>
+                                </div>
+                                <div className="songDetailsText">
+                                    <span className="songTitle">
+                                        Song Title
+                                    </span>
+                                    <span>Song Artist</span>
+                                    <span>Song Album</span>
+                                </div>
+                                <div>
+                                    <CircleButton
+                                        icon={mdiHeartOutline}
+                                        iconOnly
+                                        iconSize={1.3}
+                                    >
+                                        Add to favourites
+                                    </CircleButton>
+                                </div>
+                            </div>
+                            <div className="seekBarWrap">
+                                <SeekBar />
+                            </div>
+                            <div className="playbackButtons">
+                                <CircleButton
+                                    iconOnly
+                                    icon={mdiRepeat}
+                                    iconSize={1.1}
+                                    padding={12}
+                                >
+                                    Repeat
+                                </CircleButton>
+                                <CircleButton
+                                    iconOnly
+                                    icon={mdiSkipPreviousOutline}
+                                    iconSize={1.5}
+                                    padding={8}
+                                >
+                                    Previous
+                                </CircleButton>
+                                <CircleButton
+                                    iconOnly
+                                    icon={
+                                        getPlayerStatus === 0
+                                            ? mdiPause
+                                            : getPlayerStatus === 1
+                                            ? mdiPlay
+                                            : mdiStop
+                                    }
+                                    iconSize={2.2}
+                                    padding={14}
+                                    onClick={() => {
+                                        setPlayerStatus(
+                                            getPlayerStatus === 0 ? 1 : 0
+                                        );
+                                    }}
+                                >
+                                    Play
+                                </CircleButton>
+                                <CircleButton
+                                    iconOnly
+                                    icon={mdiSkipNextOutline}
+                                    iconSize={1.5}
+                                    padding={8}
+                                >
+                                    Next
+                                </CircleButton>
+                                <CircleButton
+                                    iconOnly
+                                    icon={mdiShuffle}
+                                    iconSize={1.1}
+                                    padding={12}
+                                >
+                                    Shuffle
+                                </CircleButton>
+                            </div>
                         </div>
-                        <div className="songDetailsText">
-                            <span class="songTitle">Song Title</span>
-                            <span>Song Artist</span>
-                            <span>Song Album</span>
-                        </div>
-                        <div>
-                            <CircleButton
-                                icon={mdiHeartOutline}
-                                iconOnly
-                                iconSize={1.3}
-                            >
-                                Add to favourites
-                            </CircleButton>
-                        </div>
-                    </div>
-                    <div className="playbackControls">
-                        <CircleButton
-                            iconOnly
-                            icon={mdiRepeat}
-                            iconSize={1.1}
-                            padding={12}
-                        >
-                            Repeat
-                        </CircleButton>
-                        <CircleButton
-                            iconOnly
-                            icon={mdiSkipPreviousOutline}
-                            iconSize={1.5}
-                            padding={8}
-                        >
-                            Previous
-                        </CircleButton>
-                        <CircleButton
-                            iconOnly
-                            icon={
-                                getPlayerStatus === 0
-                                    ? mdiPause
-                                    : getPlayerStatus === 1
-                                    ? mdiPlay
-                                    : mdiStop
-                            }
-                            iconSize={2.2}
-                            padding={14}
-                            onClick={() => {
-                                setPlayerStatus(getPlayerStatus === 0 ? 1 : 0);
-                            }}
-                        >
-                            Play
-                        </CircleButton>
-                        <CircleButton
-                            iconOnly
-                            icon={mdiSkipNextOutline}
-                            iconSize={1.5}
-                            padding={8}
-                        >
-                            Next
-                        </CircleButton>
-                        <CircleButton
-                            iconOnly
-                            icon={mdiShuffle}
-                            iconSize={1.1}
-                            padding={12}
-                        >
-                            Shuffle
-                        </CircleButton>
                     </div>
                 </TabPage>
                 <TabPage title="Playlist">Playlist</TabPage>
