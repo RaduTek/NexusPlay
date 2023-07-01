6328e4beaf4bc332cde6224d811e970ad47ed175
RaduTek <radutek13@gmail.com>
2023-07-01 23:13:48 +0300

    Implement navigation sidebar

---
+++ src/components/Layout/Sidebar/Sidebar.jsx
@@ -0,0 +1,38 @@
+import "./Sidebar.css";
+import SquareButton from "../../UI/SquareButton";
+import {
+    mdiAccountCircleOutline,
+    mdiBookshelf,
+    mdiHomeOutline,
+    mdiMagnify,
+} from "@mdi/js";
+import { useNavigate } from "react-router-dom";
+
+function Sidebar() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="libraryPane">
+            <SquareButton
+                icon={mdiHomeOutline}
+                onClick={() => {
+                    navigate("/home");
+                }}
+            >
+                Home
+            </SquareButton>
+            <SquareButton
+                icon={mdiMagnify}
+                onClick={() => {
+                    navigate("/search");
+                }}
+            >
+                Search
+            </SquareButton>
+            <SquareButton icon={mdiBookshelf}>Library</SquareButton>
+            <SquareButton icon={mdiAccountCircleOutline}>Account</SquareButton>
+        </div>
+    );
+}
+
+export default Sidebar;
