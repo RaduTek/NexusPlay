5ff9abb1c16d3c8776f25aadb2003246a720ff6c
RaduTek <radutek13@gmail.com>
2023-07-01 17:15:24 +0300

    Add atoms for now playing screen and side navigation

--- src/Atoms.js
+++ src/Atoms.js
@@ -4,3 +4,6 @@ export const playerStatus = atom(1); // Paused
 export const playerCurrentTime = atom(0);
 export const playerSeekTime = atom(0);
 export const playerTotalTime = atom(0);
+
+export const nowPlayingVisible = atom(false);
+export const sideBarVisible = atom(false);
