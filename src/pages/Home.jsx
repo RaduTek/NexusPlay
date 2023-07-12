711b5de6520e0ae4a038134171b48a7c9d235465
RaduTek <radutek13@gmail.com>
2023-07-12 23:53:13 +0300

    Remove StackGroup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,5 @@
 import Page from "../components/Layout/Page/Page";
-import {
-    StackView,
-    StackGroup,
-} from "../components/Layout/StackView/StackView";
+import { StackView } from "../components/Layout/StackView/StackView";
 import SongItem from "../components/Views/SongItem";
 import pic from "../images/Album Art.jpg";
 import { mdiHomeOutline } from "@mdi/js";
@@ -15,33 +12,31 @@ function Home() {
             <section>
                 <h2>Recently played</h2>
                 <StackView>
-                    <StackGroup>
-                        <SongItem
-                            title="Song 1"
-                            artist="Artist 1"
-                            album="Album 1"
-                            duration="4:20"
-                        />
-                        <SongItem
-                            title="Song 2"
-                            artist="Artist 2"
-                            album="Album 2"
-                            duration="4:20"
-                        />
-                        <SongItem
-                            title="Song 3"
-                            artist="Artist 3"
-                            album="Album 3"
-                            duration="4:20"
-                        />
-                        <SongItem
-                            title="Song 4"
-                            artist="Artist 4"
-                            album="Album 4"
-                            albumArt={pic}
-                            duration="4:20"
-                        />
-                    </StackGroup>
+                    <SongItem
+                        title="Song 1"
+                        artist="Artist 1"
+                        album="Album 1"
+                        duration="4:20"
+                    />
+                    <SongItem
+                        title="Song 2"
+                        artist="Artist 2"
+                        album="Album 2"
+                        duration="4:20"
+                    />
+                    <SongItem
+                        title="Song 3"
+                        artist="Artist 3"
+                        album="Album 3"
+                        duration="4:20"
+                    />
+                    <SongItem
+                        title="Song 4"
+                        artist="Artist 4"
+                        album="Album 4"
+                        albumArt={pic}
+                        duration="4:20"
+                    />
                 </StackView>
             </section>
         </Page>
