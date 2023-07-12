4a02ad24ca5122ea2fdd3fd765b05e2390b3d315
RaduTek <radutek13@gmail.com>
2023-07-12 23:53:19 +0300

    Sample Text Input

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,15 @@
 import { mdiMagnify } from "@mdi/js";
 import Page from "../components/Layout/Page/Page";
 import PageTitle from "../components/Layout/Page/PageTitle";
+import TextInput from "../components/UI/TextInput";
 
 function Search() {
     return (
         <Page>
             <PageTitle icon={mdiMagnify}>Search</PageTitle>
+            <section>
+                <TextInput placeholder="Search" />
+            </section>
         </Page>
     );
 }
