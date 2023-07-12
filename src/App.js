3546e0b951951f5e9e7c570e2f241dd025a3cb35
RaduTek <radutek13@gmail.com>
2023-07-12 23:33:16 +0300

    Implement mobile state atom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AppLayout from "./pages/AppLayout";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import Library from "./pages/Library";
+import Playlists from "./pages/Playlists";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
@@ -12,6 +15,11 @@ function App() {
                     <Route index element={<Navigate to="home" />} />
                     <Route path="home" element={<Home />} />
                     <Route path="search" element={<Search />} />
+                    <Route path="library">
+                        <Route index element={<Library />} />
+                        <Route path="playlists" element={<Playlists />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
                 </Route>
             </Routes>
         </BrowserRouter>
