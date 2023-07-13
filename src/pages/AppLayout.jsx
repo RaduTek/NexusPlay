dc24d9f8054f4e52918f59e8043da1d7410cc8ac
RaduTek <radutek13@gmail.com>
2023-07-13 21:05:11 +0300

    Add necessary changes

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,27 +2,42 @@ import { Outlet } from "react-router-dom";
 import PageHost from "../components/Layout/Page/PageHost";
 import Navbar from "../components/Layout/Navbar/Navbar";
 import AudioPlayer from "../components/Player/AudioPlayer";
-import NowPlaying from "../components/Layout/NowPlaying/NowPlaying";
+import MobileNowPlaying from "../components/Layout/NowPlaying/MobileNowPlaying";
 import Sidebar from "../components/Layout/Sidebar/Sidebar";
 import { useWindowSize } from "@uidotdev/usehooks";
 import { useEffect } from "react";
-import { useAtom } from "jotai";
-import { mobileView } from "../Atoms";
+import { useAtom, useAtomValue } from "jotai";
+import { viewType, mobilePlayingVisible } from "../Atoms";
+import MobileNavbar from "../components/Layout/Navbar/MobileNavbar";
 
 function AppLayout() {
     const windowSize = useWindowSize();
-    const [getMobileView, setMobileView] = useAtom(mobileView);
+    const [getViewType, setViewType] = useAtom(viewType);
+    const isNowPlayingVisible = useAtomValue(mobilePlayingVisible);
 
     useEffect(() => {
-        setMobileView(windowSize.width <= 550);
-    }, [windowSize, setMobileView]);
+        setViewType(windowSize.width <= 550 ? "mobile" : "full");
+    }, [windowSize, setViewType]);
 
     return (
-        <div id="app" className={(getMobileView ? "mobile" : "full") + " all"}>
+        <div
+            id="app"
+            className={
+                getViewType +
+                " " +
+                (isNowPlayingVisible ? "nowPlayingOpen" : "") +
+                " all"
+            }
+        >
             <AudioPlayer />
-            <Sidebar />
-            <NowPlaying />
+            {getViewType !== "mobile" && <Sidebar />}
             <Navbar />
+            {getViewType === "mobile" && (
+                <>
+                    <MobileNowPlaying />
+                    <MobileNavbar />
+                </>
+            )}
             <PageHost>
                 <Outlet />
             </PageHost>
